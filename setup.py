d585ae513e486ada021fdf7b35b8c391d5b41d13
matagus <matagus@gmail.com>
2010-09-18 21:47:09 -0300

    Add: App registered against pypi. And setup.py file was added.

---
+++ setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name="django-lastfmauth",
+    version=__import__("lastfmauth").__version__,
+    url="http://github.com/matagus/django-lastfm-auth",
+    license="BSD",
+    description="Django app to let your website visitors register/login using LastFM authentication webservice.",
+    long_description=open("README.md").read(),
+
+    author="Matias Agustin Mendez",
+    author_email="me@matagus.com.ar",
+
+    packages=find_packages(),
+    package_dir={"lastfmauth": "lastfmauth"},
+
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Topic :: Internet :: WWW/HTTP"
+    ],
+    zip_safe=False,
+)
